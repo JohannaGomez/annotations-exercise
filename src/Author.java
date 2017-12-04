d4ae1b5731d42d6ad952413fb8d72894fdb5f0c5
Johanna Gomez <pao_gomez1@hotmail.com>
2017-12-04 11:11:50 -0600

    no suppressed warnings used, but I changed to ArrayList format

--- src/Author.java
+++ src/Author.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private ArrayList<String> books;
+
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -20,7 +21,7 @@ public class Author extends Person {
         return books;
     }
 
-    public List<String> publishedBooks() {
+    public ArrayList<String> publishedBooks() {
         return books;
     }
 
